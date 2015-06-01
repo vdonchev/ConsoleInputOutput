12debb0a234e563c332a6ffe60a2b38e52924433
xclusivePc <xmt@abv.bg>
2015-06-01 20:31:48 +0300

    update

---
+++ NewHouse/NewHouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class NewHouse
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        int inner = 1;
+        int outter = length / 2;
+        for (int i = 0; i <= length / 2; i++)
+        {
+            Console.WriteLine("{1}{0}{1}", new string('*', inner), new string('-', outter));
+            inner += 2;
+            outter--;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine("{1}{0}{1}", new string('*', length - 2), "|");
+        }
+    }
+}
